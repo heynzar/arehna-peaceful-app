b869e98912d6a3bcb2fd01f96bea41079d2694a5
NZAR <98880087+heynzar@users.noreply.github.com>
2024-12-18 19:21:48 +0100

    ⚡ feat : rebuild app page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,23 +1,35 @@
-import audio from "@/assets/audio-lines.svg";
-import Image from "next/image";
+import { AudioLines, Settings } from "lucide-react";
 
 export default function Page() {
   return (
-    <main className="w-full h-full flex flex-col gap-2 justify-center items-center p-4 select-none">
-      <section className="bg-gradient-to-b from-[rgba(255,255,255,0.3)] to-transparent shadow-lg rounded-2xl w-full max-w-[520px] p-6 bg-clip-padding border border-white/40 backdrop-blur-[2px] flex flex-col justify-center items-center text-center">
-        <h1 className="text-[2.5rem] font-medium leading-10">THU 14 NOV</h1>
+    <main className="w-full h-full select-none flex flex-col gap-4 scale-125 justify-center items-center p-4">
+      <section className="py-4 w-full max-w-[400px] rounded-3xl  backdrop-blur-sm bg-clip-padding flex flex-col justify-center items-center text-center border-[4px] border-white/20">
+        <h1 className="text-[2.5rem] font-normal leading-10">THU 14 NOV</h1>
         <h2 className="text-8xl font-medium">12:33</h2>
       </section>
 
-      <section className="flex items-center w-full max-w-[520px] gap-2">
-        <button className="button-gradient" aria-label="Play Quran sounds">
+      <section className="flex items-center w-full max-w-[400px] gap-2">
+        <button
+          className="key__button-2 flex items-center justify-center gap-2 px-2 py-5 w-full"
+          aria-label="Play Quran sounds"
+        >
           <span className="text-lg font-medium">Quran</span>
-          <Image src={audio} alt="Audio icon" width={24} height={24} />
+          <AudioLines />
         </button>
 
-        <button className="button-gradient" aria-label="Play relaxing noise">
-          <span className="text-lg font-medium">Noise</span>
-          <Image src={audio} alt="Audio icon" width={24} height={24} />
+        <button
+          className="key__button-2 flex items-center justify-center gap-2 px-2 py-5 w-full"
+          aria-label="Play Quran sounds"
+        >
+          <span className="text-lg font-medium">Sounds</span>
+          <AudioLines />
+        </button>
+
+        <button
+          className="key__button-2 flex items-center justify-center gap-2 px-2 py-5 w-min"
+          aria-label="Play Quran sounds"
+        >
+          <Settings />
         </button>
       </section>
     </main>
---
+++ src/archive/AppDesgn.tsx
@@ -0,0 +1,56 @@
+{
+  /* <section className="flex bg-gradient-to-b from-[rgba(255,255,255,0.3)] to-transparent shadow-lg rounded-2xl w-full max-w-[400px] p-6 bg-clip-padding border border-white/40 backdrop-blur-[2px]  flex-col justify-center items-center text-center">
+        <h1 className="text-[2.5rem] font-medium leading-10">THU 14 NOV</h1>
+        <h2 className="text-8xl font-medium">12:33</h2>
+      </section> */
+}
+
+{
+  /* <section className="flex items-center w-full max-w-[400px] gap-2">
+        <button className="button-gradient" aria-label="Play Quran sounds">
+          <span className="text-lg font-medium">Quran</span>
+          <Image src={audio} alt="Audio icon" width={24} height={24} />
+        </button>
+
+        <button className="button-gradient" aria-label="Play relaxing noise">
+          <span className="text-lg font-medium">Noise</span>
+          <Image src={audio} alt="Audio icon" width={24} height={24} />
+        </button>
+      </section> */
+}
+
+{
+  /* <section className="flex items-center w-full max-w-[400px] gap-2">
+        <button
+          onClick={() => {
+            setOpenQuran(true);
+          }}
+          className="w-full active:scale-95 flex items-center gap-2 bg-neutral-900/60 backdrop-blur-sm p-2 rounded-xl hover:bg-neutral-900/70 transition-all duration-300 justify-center"
+          aria-label="Play Quran sounds"
+        >
+          <span className="text-lg">Quran</span>
+          <Image src={audio} alt="Audio icon" width={24} height={24} />
+        </button>
+
+        <button
+          onClick={() => {
+            setOpenSounds(true);
+          }}
+          className="w-full active:scale-95 flex items-center gap-2 bg-neutral-900/60 backdrop-blur-sm p-2 rounded-xl hover:bg-neutral-900/70 transition-all duration-300 justify-center "
+          aria-label="Play relaxing noise"
+        >
+          <span className="text-lg">Noise</span>
+          <Image src={audio} alt="Audio icon" width={24} height={24} />
+        </button>
+
+        <button
+          onClick={() => {
+            setOpenSettings(true);
+          }}
+          className="w-min active:scale-95 flex items-center gap-2 bg-neutral-900/60 backdrop-blur-sm p-2 rounded-xl hover:bg-neutral-900/70 transition-all duration-300 justify-center "
+          aria-label="Settings"
+        >
+          <Settings strokeWidth={1} />
+        </button>
+      </section> */
+}
