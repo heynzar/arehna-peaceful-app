9648989162bb3671c0fc338947f4dbc7d64fdb31
NZAR <98880087+heynzar@users.noreply.github.com>
2024-12-18 18:26:37 +0100

    ⚡ feat : add Settings sections

---
+++ src/app/components/Settings.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import Image from "next/image";
+import bg1 from "@/assets/bg1-square.png";
+import bg2 from "@/assets/bg2-square.png";
+import bg3 from "@/assets/bg3-square.png";
+import { useState } from "react";
+import { X } from "lucide-react";
+
+export default function Settings() {
+  const [openSettings, setOpenSettings] = useState(false);
+  const [check, setCheck] = useState(false);
+  const [hijriDate, setHijriDate] = useState(false);
+  return (
+    <section
+      className={`justify-center  items-center absolute inset-0 w-screen h-screen bg-black/20 backdrop-blur-sm ${
+        openSettings ? "flex" : "hidden"
+      }`}
+    >
+      <div className="relative p-4 w-full max-w-[496px] scale-110 rounded-2xl bg-neutral-900 border border-white/15">
+        <button
+          onClick={() => {
+            setOpenSettings(false);
+          }}
+          className="bg-neutral-800 p-1 rounded-lg ml-auto absolute right-4 top-4"
+        >
+          <X className="hover:text-white/80" />
+        </button>
+
+        <div className="flex flex-col">
+          <div className="mt-2">
+            <span className="">App Settings</span>
+            <div className="my-2 flex justify-between items-center bg-neutral-800 py-1 px-2 rounded-lg">
+              <span className="text-sm opacity-80">Toggole Hijri Date</span>
+              <div
+                onClick={() => setHijriDate(!hijriDate)}
+                className="w-9 cursor-pointer rounded-full p-0.5 bg-neutral-900"
+              >
+                <div
+                  className={`${
+                    hijriDate ? "bg-sky-400 translate-x-4" : "bg-neutral-600"
+                  } size-4 rounded-full transition-all ease-in-out duration-300`}
+                ></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-4 flex justify-between items-center bg-neutral-800 py-1 px-2 rounded-lg">
+            <span className="text-sm opacity-80">
+              Toggle Quran Playing Method
+            </span>
+            <div
+              onClick={() => setCheck(!check)}
+              className="w-9 cursor-pointer rounded-full p-0.5 bg-neutral-900"
+            >
+              <div
+                className={`${
+                  check ? "bg-sky-400 translate-x-4" : "bg-neutral-600"
+                } size-4 rounded-full transition-all ease-in-out duration-300`}
+              ></div>
+            </div>
+          </div>
+        </div>
+        <span className="">Backgrounds</span>
+        <div
+          id="backgounds"
+          className="pl-1 flex flex-wrap gap-2 justify-start items-center mt-2"
+        >
+          <div className="h-[54px] aspect-video outline outline-2 outline-offset-2 outline-sky-400 cursor-pointer hover:bg-neutral-700 transition-colors duration-300 flex items-center justify-center rounded-lg bg-neutral-800 overflow-hidden">
+            <Image src={bg1} alt="background image 1" className="bg-contain" />
+          </div>
+          <div className="h-[60px] aspect-video cursor-pointer hover:bg-neutral-700 transition-colors duration-300 flex items-center justify-center rounded-lg bg-neutral-800 overflow-hidden">
+            <Image src={bg2} alt="background image 1" className="bg-contain" />
+          </div>
+          <div className="h-[60px] aspect-video cursor-pointer hover:bg-neutral-700 transition-colors duration-300 flex items-center justify-center rounded-lg bg-neutral-800 overflow-hidden">
+            <Image src={bg2} alt="background image 1" className="bg-contain" />
+          </div>
+          <div className="h-[60px] aspect-video cursor-pointer hover:bg-neutral-700 transition-colors duration-300 flex items-center justify-center rounded-lg bg-neutral-800 overflow-hidden">
+            <Image src={bg3} alt="background image 1" className="bg-contain" />
+          </div>
+        </div>
+
+        <div className="mt-4 w-full flex justify-between items-end">
+          <button className="px-6 py-0.5 bg-sky-400 rounded-lg">Save</button>
+          <p className="text-[0.7rem]">
+            Develop by{" "}
+            <a href="" className="underline">
+              Nzar
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/lib/data.ts
@@ -0,0 +1,17 @@
+export const quran = [
+  {
+    name: "إن المتقين في مقام أمين",
+    src: "/ding.mp3",
+  },
+  {
+    name: "ولمن خاف مقام ربه جنتان",
+    src: "/ding.mp3",
+  },
+];
+
+export const sounds = [
+  {
+    // name: Bird,
+    src: "/bird.mp3",
+  },
+];
