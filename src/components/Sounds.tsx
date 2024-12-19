2de948f7228a91d8fecd721c27598a7811e2f613
NZAR <98880087+heynzar@users.noreply.github.com>
2024-12-19 11:54:31 +0100

    ⚡ feat :  add logic to sounds section

--- src/components/Sounds.tsx
+++ src/components/Sounds.tsx
@@ -1,19 +1,8 @@
 "use client";
-import { SetStateAction } from "react";
-import {
-  Bird,
-  CloudLightning,
-  CloudRain,
-  Coffee,
-  Droplets,
-  Flame,
-  MoonStar,
-  Volume2,
-  Waves,
-  Wheat,
-  Wind,
-  X,
-} from "lucide-react";
+import { SetStateAction, useState, useRef } from "react";
+import { Volume2, X } from "lucide-react";
+
+import { sounds } from "@/lib/data";
 export default function Sounds({
   open,
   setOpen,
@@ -21,6 +10,31 @@ export default function Sounds({
   open: boolean;
   setOpen: (value: SetStateAction<boolean>) => void;
 }) {
+  const audioRefs = useRef<{ [key: string]: HTMLAudioElement | null }>({});
+  const [playing, setPlaying] = useState<{ [key: string]: boolean }>({});
+
+  const toggleAudio = (src: string) => {
+    // Initialize audio if it doesn't exist
+    if (!audioRefs.current[src]) {
+      const audio = new Audio(src);
+      audio.loop = true; // Set audio to loop
+      audioRefs.current[src] = audio;
+    }
+
+    const audio = audioRefs.current[src]!;
+
+    if (playing[src]) {
+      // If the audio is currently playing, pause it
+      audio.pause();
+      setPlaying((prev) => ({ ...prev, [src]: false }));
+    } else {
+      // Otherwise, play the audio
+      audio.play().catch((error) => {
+        console.error("Audio playback failed:", error);
+      });
+      setPlaying((prev) => ({ ...prev, [src]: true }));
+    }
+  };
   return (
     <section
       className={`justify-center items-center absolute inset-0 w-screen h-screen bg-black/20 backdrop-blur-sm ${
@@ -30,7 +44,9 @@ export default function Sounds({
       <div className="p-4 w-full max-w-[496px] scale-110 rounded-2xl bg-zinc-950 border border-zinc-800">
         <div className="flex justify-between w-full">
           <div className="flex items-center gap-4 w-[240px] rounded-lg bg-zinc-900 py-1 px-4">
-            <Volume2 className="cursor-pointer hover:text-white/80" />
+            <button>
+              <Volume2 className="hover:text-white/80" />
+            </button>
             <input type="range" className="w-full" />
           </div>
           <button
@@ -46,39 +62,19 @@ export default function Sounds({
           id="sounds"
           className="flex flex-wrap gap-2 justify-center items-center mt-4"
         >
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <Bird size={40} strokeWidth={1} />
-          </div>
-          <div
-            // onClick={playAudio}
-            className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-sky-400"
-          >
-            <CloudRain size={40} strokeWidth={1} />
-          </div>
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <CloudLightning size={40} strokeWidth={1} />
-          </div>
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <MoonStar size={40} strokeWidth={1} />
-          </div>
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <Flame size={40} strokeWidth={1} />
-          </div>
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <Coffee size={40} strokeWidth={1} />
-          </div>
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <Droplets size={40} strokeWidth={1} />
-          </div>
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <Wind size={40} strokeWidth={1} />
-          </div>
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <Waves size={40} strokeWidth={1} />
-          </div>
-          <div className="size-[86px] cursor-pointer hover:bg-zinc-800 transition-colors duration-300 flex items-center justify-center rounded-lg bg-zinc-900">
-            <Wheat size={40} strokeWidth={1} />
-          </div>
+          {sounds.map(({ Icon, src }) => (
+            <button
+              onClick={() => toggleAudio(src)}
+              key={src}
+              className={`size-[86px] cursor-pointer  transition-colors duration-300 flex items-center justify-center rounded-lg ${
+                playing[src]
+                  ? "bg-sky-500 hover:bg-sky-400"
+                  : "bg-zinc-900 hover:bg-zinc-800"
+              }`}
+            >
+              <Icon size={40} strokeWidth={1} />
+            </button>
+          ))}
         </div>
       </div>
     </section>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,17 +1,109 @@
+import {
+  Bird,
+  CloudLightning,
+  CloudRain,
+  Coffee,
+  Droplets,
+  Flame,
+  MoonStar,
+  Waves,
+  Dam,
+  Wind,
+} from "lucide-react";
+
+// export const quran = [
+//   {
+//     name: "إن المتقين في مقام أمين",
+//     src: "/ding.mp3",
+//   },
+//   {
+//     name: "ولمن خاف مقام ربه جنتان",
+//     src: "/ding.mp3",
+//   },
+// ];
+
 export const quran = [
   {
-    name: "إن المتقين في مقام أمين",
-    src: "/ding.mp3",
+    name: "سورة البقرة",
+    src: "https://server6.mp3quran.net/qtm/002.mp3",
   },
   {
-    name: "ولمن خاف مقام ربه جنتان",
-    src: "/ding.mp3",
+    name: "سورة الملك",
+    src: "https://server6.mp3quran.net/qtm/067.mp3",
+  },
+  {
+    name: "سورة يوسف",
+    src: "https://server6.mp3quran.net/qtm/012.mp3",
+  },
+  {
+    name: "سورة النساء",
+    src: "https://server6.mp3quran.net/qtm/004.mp3",
+  },
+  {
+    name: "سورة الكهف",
+    src: "https://server6.mp3quran.net/qtm/018.mp3",
+  },
+  {
+    name: "سورة الصافات",
+    src: "https://server6.mp3quran.net/qtm/037.mp3",
+  },
+  {
+    name: "سورة الرحمن",
+    src: "https://server6.mp3quran.net/qtm/055.mp3",
+  },
+  {
+    name: "سورة الواقعة",
+    src: "https://server6.mp3quran.net/qtm/056.mp3",
+  },
+  {
+    name: "سورة يس",
+    src: "https://server6.mp3quran.net/qtm/036.mp3",
+  },
+  {
+    name: "سورة طه",
+    src: "https://server6.mp3quran.net/qtm/020.mp3",
   },
 ];
 
 export const sounds = [
   {
-    // name: Bird,
-    src: "/bird.mp3",
+    Icon: Bird,
+    src: "https://upbase.io/_assets/pomoup/1_Birds.mp3",
+  },
+  {
+    Icon: CloudRain,
+    src: "https://upbase.io/_assets/pomoup/2_Rain.mp3",
+  },
+  {
+    Icon: CloudLightning,
+    src: "https://upbase.io/_assets/pomoup/3_Thunder.mp3",
+  },
+  {
+    Icon: Droplets,
+    src: "/audios/drops.mp3",
+  },
+  {
+    Icon: Dam,
+    src: "/audios/waterfall2.mp3",
+  },
+  {
+    Icon: Waves,
+    src: "https://upbase.io/_assets/pomoup/8_Ocean_Waves.mp3",
+  },
+  {
+    Icon: Coffee,
+    src: "https://upbase.io/_assets/pomoup/4_Coffee_Shop.mp3",
+  },
+  {
+    Icon: Wind,
+    src: "/audios/wind2.mp3",
+  },
+  {
+    Icon: Flame,
+    src: "https://upbase.io/_assets/pomoup/6_Fire.mp3",
+  },
+  {
+    Icon: MoonStar,
+    src: "/audios/night2.mp3",
   },
 ];
